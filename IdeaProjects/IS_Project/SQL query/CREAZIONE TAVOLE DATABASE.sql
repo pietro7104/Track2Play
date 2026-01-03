508f49e81aaeb398927381f9a5d0ae236910cd7d
Emanuele Rosario Fusaro <emanuelefusaro16@gmail.com>
2026-01-03 16:24:13 +0100

    aggiunto codice sql per la creazione delle tabelle

--- IdeaProjects/IS_Project/SQL query/CREAZIONE TAVOLE DATABASE.sql
+++ IdeaProjects/IS_Project/SQL query/CREAZIONE TAVOLE DATABASE.sql
@@ -1,5 +1,51 @@
-CREATE TABLE User(
-	ID int PRIMARY KEY,
-    username varchar(100) UNIQUE,
-    password varchar(300)
-)
+CREATE TABLE Utente (
+	IdUtente int auto_increment primary key,
+    Username varchar(30) unique not null,
+    Password varchar(128) not null
+);
+
+CREATE TABLE Gioco (
+	IdGioco varchar(36) primary key,
+    Titolo varchar(100) not null,
+    Copertina varchar(150) not null
+);
+
+CREATE TABLE Acquisto (
+	Data_Acquisto timestamp,
+    IdUtente int references Utente(IdUtente),
+    IdGioco varchar(36) references Gioco(IdGioco),
+    Piattaforma varchar(50) not null,
+    Prezzo double not null,
+    Regalo boolean not null,
+    primary key(Data_Acquisto, IdUtente, IdGioco)
+);
+
+CREATE TABLE Wishlist(
+	IdUtente int references Utente(IdUtente),
+    IdGioco varchar(36) references Gioco(IdGioco),
+    Data_Aggiunta DATE not null,
+    primary key(IdUtente, IdGioco)
+);
+
+CREATE TABLE Collezione(
+	IdUtente int references Utente(IdUtente),
+    Num_Tot int default 0 not null,
+    Num_Completati int default 0 not null,
+    Num_NonCompletati int default 0 not null,
+    primary key(IdUtente)
+);
+
+CREATE TABLE Aggiunto(
+	IdUtente int references Utente(IdUtente),
+    IdGioco varchar(36) references Gioco(IdGioco),
+    Data_Aggiunta DATE not null,
+    primary key(IdUtente, IdGioco)
+);
+
+CREATE TABLE Completamento (
+	IdUtente int references Utente(IdUtente),
+    IdGioco varchar(36) references Gioco(IdGioco),
+    Data_Completamento DATE not null,
+    Stato boolean not null,
+    primary key(IdUtente, IdGioco)
+);
