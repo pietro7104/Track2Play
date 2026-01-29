7850a1c0f677f33018a9a1369e0460856a6edce6
Abhay <pintaabhay11@gmail.com>
2026-01-29 23:19:36 +0100

    Aggiunta LoginServletTest

---
+++ IdeaProjects/IS_Project/src/test/java/Controller/Login/LoginServletTest.java
@@ -0,0 +1,100 @@
+package Controller.Login;
+
+import Controller.AccountManagement.LoginServlet;
+import Model.User;
+import Model.UserManagement;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class LoginServletTest {
+
+    @Test
+    void testLoginUsernameNotRegistered() throws ServletException, IOException {
+
+        HttpSession session = mock(HttpSession.class);
+        RequestDispatcher rd = mock(RequestDispatcher.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        UserManagement userManagement = mock(UserManagement.class);
+
+
+        when(request.getParameter("username")).thenReturn("user_not_registered");
+        when(request.getParameter("password")).thenReturn("password");
+        when(request.getSession()).thenReturn(session);
+        when(request.getRequestDispatcher(Mockito.anyString())).thenReturn(rd);
+
+
+        when(userManagement.checkCredentialsAndGetUser("user_not_registered", "password"))
+                .thenThrow(new SQLException("User not found"));
+
+        HashMap<String, Object> attributes = new HashMap<>();
+        doAnswer(new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                attributes.put(args[0].toString(), args[1]);
+                return null;
+            }
+        }).when(request).setAttribute(Mockito.anyString(), Mockito.any(Object.class));
+
+
+        LoginServlet loginServlet = new LoginServlet();
+        loginServlet.doPost(request, response);
+
+
+        ArrayList<String> errors = (ArrayList<String>) attributes.get("error_list");
+        assert(errors != null && !errors.isEmpty() && errors.get(0).equals("L'username non è registrato"));
+
+
+        verify(request).getRequestDispatcher("index.jsp");
+        verify(rd).forward(request, response);
+    }
+
+    @Test
+    void testLoginCorrect() throws ServletException, IOException {
+
+        HttpSession session = mock(HttpSession.class);
+        RequestDispatcher rd = mock(RequestDispatcher.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        UserManagement userManagement = mock(UserManagement.class);
+
+
+        when(request.getParameter("username")).thenReturn("user_registered");
+        when(request.getParameter("password")).thenReturn("correct_password");
+        when(request.getSession()).thenReturn(session);
+        when(request.getRequestDispatcher(Mockito.anyString())).thenReturn(rd);
+
+
+        User user = new User();
+        user.setUsername("user_registered");
+        user.setPassword("correct_password");
+
+        when(userManagement.checkCredentialsAndGetUser("user_registered", "correct_password"))
+                .thenReturn(user);
+
+
+        LoginServlet loginServlet = new LoginServlet();
+        loginServlet.doPost(request, response);
+
+
+        verify(session).setAttribute("user", user);
+        verify(request).getRequestDispatcher("Home Page.jsp");
+        verify(rd).forward(request, response);
+    }
+}
+
